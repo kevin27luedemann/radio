02423369a8d7b20a4725248bd3a63fcc90b67208
Kevin Luedemann <kevin27luedemann@gmail.com>
2016-04-15 22:55:04 +0200

    datei kann jetz auch gespeichert werden

--- prog/TTS/pico/wetter.sh
+++ prog/TTS/pico/wetter.sh
@@ -8,11 +8,11 @@ fi
 
 if [ $stunde -gt 4 ]; then
 	if [ $stunde -le 9 ] ; then
-		wget -N -q -P /home/pi/ http://www.wetter.com/deutschland/stemmen/DE0010191.html
-		text1="`cat /home/pi/DE0010191.html | awk '/In Stemmen/ {print $0}' | head -1`"
+		wget --directory-prefix=/home/$USER/ -q http://www.wetter.com/deutschland/stemmen/DE0010191.html
+		text1="`cat /home/$USER/DE0010191.html | awk '/In Stemmen/ {print $0}' | head -1`"
 		text=`echo ${text1:0:-6}`
-		./sprich.sh "Guten Morgen." #" $text"
-		rm -f DE0010191.html
+		./sprich.sh "Guten Morgen. $text"
+		rm -f /home/$USER/DE0010191.html
 	fi
 fi
 
