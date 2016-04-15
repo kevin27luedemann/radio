4b5ff5739a1ff530e18f98b649b83d2f0f6e37da
kevin.luedemann <kevin.luedemann@stud.uni-goettingen.de>
2016-04-15 17:53:35 +0200

    TTS hinzu

---
+++ prog/TTS/TTStes.sh
@@ -0,0 +1,3 @@
+#Test der SSH Funktion, um sie dann in ein System fuer Fred einzubauen
+./TTSGoogle.sh Das ist ein Test Text, mit dem ich das TTS teste.
+#This is an test Text for testing the TTS funktion.
