02f5c39a947355f0b59e9a985fa2f061d813aa6e
Kevin Luedemann <kevin27luedemann@gmail.com>
2016-04-16 17:46:38 +0200

    Fehler in Wetter und sprich behoben

--- prog/TTS/pico/sprich.sh
+++ prog/TTS/pico/sprich.sh
@@ -6,5 +6,5 @@ tempfile="/tmp/sprich.wav"
 text="$1"
 
 pico2wave -l=${sprache} -w=${tempfile} "`echo ${text}`"
-aplay ${tempfile} 2> /dev/null
+aplay -q ${tempfile} 2> /dev/null
 rm -f ${tempfile}
--- prog/TTS/pico/wetter.sh
+++ prog/TTS/pico/wetter.sh
@@ -6,13 +6,13 @@ if [ "$1" == "radio" ]; then
 	mpc stop
 fi
 
-if [ $stunde -gt 4 ]; then
+if [ $stunde -ge 5 ]; then
 	if [ $stunde -le 9 ] ; then
-		wget --directory-prefix=/home/$USER/ -q http://www.wetter.com/deutschland/stemmen/DE0010191.html
-		text1="`cat /home/$USER/DE0010191.html | awk '/In Stemmen/ {print $0}' | head -1`"
+		wget --directory-prefix=/tmp/ -q http://www.wetter.com/deutschland/stemmen/DE0010191.html
+		text1="`cat /tmp/DE0010191.html | awk '/In Stemmen/ {print $0}' | head -1`"
 		text=`echo ${text1:0:-6}`
-		./sprich.sh "Guten Morgen. $text"
-		rm -f /home/$USER/DE0010191.html
+		/usr/scripte/sprich.sh "Guten Morgen. $text"
+		rm -f /tmp/DE0010191.html
 	fi
 fi
 
