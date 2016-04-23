88af2283c46fd1e050594d268a9691cf4363a4b5
kevin.luedemann <kevin.luedemann@stud.uni-goettingen.de>
2016-04-23 14:31:45 +0200

    install verbessert

--- prog/radio/install.sh
+++ prog/radio/install.sh
@@ -6,15 +6,16 @@ mkdir -p /usr/bin/piradio/;
 cp -p ./ress/*.jpg /usr/bin/piradio/;
 cp -p ./ress/*.png /usr/bin/piradio/;
 cp radio /usr/bin/;
-mkdir -p /tmp/piradio/
-chmod 777 /tmp/piradio/
+rm -f radio;
+mkdir -p /tmp/piradio/;
+chmod 777 /tmp/piradio/;
 echo "test radio" > /tmp/piradio/stat.txt;
 chmod 777 /tmp/piradio/stat.txt;
 
 # kopieren der notwendigen scripte
-mkdir -p /usr/scripte/
-cp -p ../TTS/pico/sprich.sh /usr/scripte/
-chmod 555 /usr/scripte/sprich.sh
-cp -P ../TTS/pico/wetter.sh /usr/scripte/
-chmod 555 /usr/scripte/wetter.sh
+mkdir -p /usr/scripte/;
+cp -p ../TTS/pico/sprich.sh /usr/scripte/;
+chmod 555 /usr/scripte/sprich.sh;
+cp -P ../TTS/pico/wetter.sh /usr/scripte/;
+chmod 555 /usr/scripte/wetter.sh;
 #radio
