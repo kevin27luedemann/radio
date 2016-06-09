d7abe35a5675df02b8a106e5100d997b7d786dfb
kevin.luedemann <kevin.luedemann@stud.uni-goettingen.de>
2016-06-09 07:34:14 +0200

    Fehler in Ausgabe Wetter behoben

--- prog/wetter/humidity/script/ausgabe_wetter.sh
+++ prog/wetter/humidity/script/ausgabe_wetter.sh
@@ -4,5 +4,5 @@ datum=`date +"%d.%m.%y"`
 
 mv ~/.wetter.log ./
 
-gnuplot -e "DATUM='/home/pi/Bilder/wetter/$datum.pdf'" plot.gp
+gnuplot -e "DATUM='/home/pi/Bilder/wetter/$datum.pdf'" /home/pi/projekt/radio/prog/wetter/humidity/script/plot.gp
 mv .wetter.log /home/pi/Bilder/wetter/.$datum.log
---
+++ prog/wetter/humidity/script/demo_ausgabe_wetter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# grundlegende Zeitvariablen
+datum=`date +"%d.%m.%y"`
+datum=09.06.16
+
+#mv ~/.wetter.log ./
+
+gnuplot -e "DATUM='/home/pi/Bilder/wetter/$datum.pdf'" plot.gp
+#mv .wetter.log /home/pi/Bilder/wetter/.$datum.log
