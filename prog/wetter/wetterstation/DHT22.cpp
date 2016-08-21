b07d2d0033e2ce7dba4cd5b9b825232ce2715f2b
Kevin Luedemann <kevin27luedemann@googlemail.com>
2016-08-21 17:00:25 +0200

    Funktionsfahige Klasse fuer einen Sensor. Optimierung geht spaeter los

--- prog/wetter/wetterstation/DHT22.cpp
+++ prog/wetter/wetterstation/DHT22.cpp
@@ -1,129 +1,228 @@
+/* Copyright 2014 Moreto
+ *
+ * This file is part of DHT22 Interrupt Driven library for AVR.
+ *
+ * DHT22 Interrupt Driven library for AVR is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * DHT22 Interrupt Driven library for AVR is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ * 
+ * Please consult the GNU General Public License at http://www.gnu.org/licenses/.
+ */
 
-
+/*
+ * DHT22int.c
+ * 
+ * Version 1
+ *
+ * Main file of the DHT22 Interrupt Driven library for AVR.
+ * Created: 12/01/2014 22:26:03
+ * Author: Miguel Moreto
+ 
+ * This lib can read temperature and humidity of a DHT22 sensor without
+ * blocking the microcontroller with unnecessary delay functions.
+ * The lib is interrupt driven, all the timing measurements of sensor
+ * signal is done with a timer and a external interrupt using a state machine.
+ * This way, you can use this lib with multiplexed displays without flicker the
+ * display during the measurement of the sensor data.
+ *
+ * REQUIREMENTS: 
+ *   A pin with external interrupt (INT0, INT1 or other).
+ *   A timer with Clear Timer on Compare Match mode (CTC).
+ *   Timer prescaler that gives a timer frequency of 1MHz.
+ *
+ *   Pin change interrupt is not supported yet.
+ *
+ * HOW IT WORKS:
+ * Check the comments in this file to fully understand how it works. Basically:
+ *   1) A timer in CTC is used to generate the host start condition.
+ *      pin is configured as output (this is done in the timer interrupt
+ *      handler function).
+ *   2) Pin is switched to input with external interrupt. At each external
+ *      interrupt the number of timer ticks (configured to occur at each
+ *      microsecond) is counted.
+ *   3) The value of the counter (microseconds) is compared with a fixed
+ *      value in a state machine, this way, the signal from DHT22 is
+ *      interpreted. This is done at the External Interrupt Handler.
+ *
+ * HOW TO USE:
+ *  Include the lib:
+ *
+ *      #include "DHT22int.h"
+ *
+ *  Before entering the main loop of your program, declare some needed variables
+ *  call the init function and enable interrupts.
+ *
+ *      DHT22_STATE_t state;
+ *      DHT22_DATA_t sensor_data;
+ *      DHT22_Init();
+ *      sei();
+ *
+ *  Periodically (or not, depending of your use), call the function to 
+ *  start reading the sensor:
+ *
+ *      state = DHT22_StartReading();
+ *
+ *  Check the state if you want to confirm that the state machine has started.
+ *  In your main loop, check periodically when the data is available and in
+ *  case of available, do something:
+ *
+ *      state = DHT22_CheckStatus(&sensor_data);
+ *
+ * 		if (state == DHT_DATA_READY){
+ *	 		// Do something with the data.
+ *          // sensor_data.temperature_integral
+ *          // sensor_data.temperature_decimal
+ *          // sensor_data.humidity_integral
+ *          // sensor_data.humidity_decimal
+ * 		}
+ *		else if (state == DHT_ERROR_CHECKSUM){
+ *	 		// Do something if there is a Checksum error
+ *		}
+ * 		else if (state == DHT_ERROR_NOT_RESPOND){
+ *	 		// Do something if the sensor did not respond
+ * 		}
+ *
+ *  To start a new measurement, you have to call DHT22_StartReading() again.
+ *
+ *  IMPORTANT: You need to modify the header (.h) file accordingly with your 
+ *             microcontroller, the external interrupt used (and the pin) and
+ *             the timer.
+ */ 
 
 #include "DHT22.h"
 
-#define DHT22_COUNT 6
-#define DHT22_MAXTIMINGS 85
-
+/*
+ * void DHT22_Init(void)
+ *
+ * Function to be called before the main loop.
+ * It configures the sensor pin and timer mode.
+ */
 DHT22::DHT22(uint8_t DDRpin){
 	pin	 = DDRpin;
 	temperature_decimal = 0;
 	temperature_integral = 0;
 	humidity_decimal = 0;
 	humidity_integral = 0;
-	for(uint8_t i=0;i<DHT22_COUNT;i++){
-		data[i] = 0;
-	}
+
+	/* Configuring DHT pin as output (initially) */
+	DHT22_DDR |= (1 << DHT22_PIN);
+	PIN_HIGH(DHT22_PORT,DHT22_PIN);
+	
+	/* Timer config. */
+	TIMER_SETUP_CTC  // Seting timer to CTC
+	TIMER_ENABLE_CTC_INTERRUPT  // Enable compare match interrupt
+	// Timer is started by the function DHT22_StartReading. For now
+	// it remains with prescaler = 0 (disable).
+	TIMER_STOP
+	
+	state = DHT_STOPPED;
+
+	overflow_cnt = 0;
+	bitcounter = 0;
+
+	rawHumidity = 0;
+	rawTemperature = 0;
+	checkSum = 0;
+	
 }
 
 DHT22::~DHT22(){
 }
 
-uint8_t DHT22::count_seconds(){
-	uint8_t counter = 0;
-	while(!(PIN_DHT22&(1<<pin)) && counter < 255){
-		_delay_us(1);
-		counter ++;
+/* NOTE: Check the macro definitions at the header file. */
+
+
+
+/*
+ * DHT22_STATE_t DHT22_CheckStatus(DHT22_DATA_t* data)
+ *
+ * Function that should be called after DHT22_StartReading() in order to check
+ * if a transfer is complete.
+ *
+ * It returns a DHT22_STATE_t variable with the state of the state machine.
+ *  Returned values:
+ *    DHT_DATA_READY: Data is ok and can be used by the main program.
+ *    DHT_ERROR_CHECKSUM: Error, checksum does no match.
+ *    DHT_ERROR_NOT_RESPOND: Sensor is not connected or not responding for some reason.
+ */
+
+uint8_t DHT22::DHT22_CheckStatus(){
+	
+	/* If a transfer is complete, check CRC and update sensor data structure */
+	if (state == DHT_CHECK_CRC){
+		
+		// calculate checksum:
+		csPart1 = rawHumidity >> 8;
+		csPart2 = rawHumidity & 0xFF;
+		csPart3 = rawTemperature >> 8;
+		csPart4 = rawTemperature & 0xFF;
+		
+		if( checkSum == ( (csPart1 + csPart2 + csPart3 + csPart4) & 0xFF ) ){ // Checksum correct
+			
+			/* raw data to sensor values */
+			humidity_integral = (uint8_t)(rawHumidity / 10.0)+15;
+			humidity_decimal = (uint8_t)(rawHumidity % 10);			
+			if(rawTemperature & 0x8000)	// Check if temperature is below zero, non standard way of encoding negative numbers!
+			{
+				rawTemperature &= 0x7FFF; // Remove signal bit
+				temperature_integral = (int8_t)(rawTemperature / 10.0) * -1;
+				temperature_decimal = (uint8_t)(rawTemperature % 10);
+			} else
+			{
+				temperature_integral = (int8_t)(rawTemperature / 10.0);
+				temperature_decimal = (uint8_t)(rawTemperature % 10);
+			}
+			state = DHT_DATA_READY;
+		}
+		else{
+			state = DHT_ERROR_CHECKSUM;
+		}
 	}
-	if(counter >=255 ){
-		return 212;
+	
+	return state;
+}
+
+/*
+ * DHT22_STATE_t DHT22_StartReading(void)
+ *
+ * This function starts a new reading of the sensor.
+ * It returns a variable of type DHT22_STATE_t with the possible values:
+ *    DHT_BUSY: The reading did not started because the state machine 
+ *              is doing something else, indicating that the previous
+ *              reading did not finished.
+ *    DHT_STARTED: The state machine has successfully started. The user
+ *                 can wait for data using DHT22_CheckStatus() function.
+ *
+ */
+uint8_t DHT22::DHT22_StartReading(void){
+	
+	/* Check if the state machine is stopped. If so, start it. */
+	if (state == DHT_STOPPED || state == DHT_DATA_READY || state == DHT_ERROR_CHECKSUM || state == DHT_ERROR_NOT_RESPOND){
+		/* Reset values and counters */
+		rawTemperature = 0;
+		rawHumidity = 0;
+		checkSum = 0;
+		overflow_cnt = 0;
+		bitcounter = 0;
+		/* Configuring peripherals */
+		//EIMSK &= ~(1 << INT0); // Disable external interrupt
+		EXT_INTERRUPT_DISABLE
+		DHT22_DDR |= (1 << DHT22_PIN); // Configuring sensor pin as output.
+		PIN_LOW(DHT22_PORT,DHT22_PIN); // Write 0 to pin. Start condition sent to sensor.
+		TIMER_OCR_REGISTER = 255; // Timer compare value equals to overflow (interrupt will fired at 255us).
+		TIMER_COUNTER_REGISTER = 0; // Reset counter value.
+		state = DHT_HOST_START; // Change state.
+		TIMER_START // Start timer with prescaler such that 1 tick equals 1us (freq = 1MHz).
+		return DHT_STARTED; // Return value indicating that the state machine started.
 	}
 	else{
-		counter = 0;
+		return DHT_BUSY; // If state machine is busy, return this value.
 	}
-	while((PIN_DHT22&(1<<pin)) && counter < 255){
-		_delay_us(1);
-		counter++;
-	}
-	return counter;
-}
-
-uint8_t DHT22::read_sensor(){
-    uint8_t sum = 0;
-    uint8_t counter = 0;
-    data[0] = data[1] = data[2] = data[3] = data[4] = 0;
-
-	//sende startsignal
-    DDR_DHT22 |= (1 << pin);
-    PORT_DHT22 &= ~(1 << pin);
-    _delay_ms(10);
-    cli();
-    PORT_DHT22 |= (1 << pin);
-    DDR_DHT22 &= ~(1 << pin);
-	_delay_us(2);
-	//warte antwort ab
-	while((PIN_DHT22&(1<<pin)) && counter < 255){
-		counter ++;
-	}
-	if(counter == 255){
-		sei();
-		return 4;
-	}
-	counter = count_seconds();
-	if(counter >= 250){
-		sei();
-		return 3;
-	}
-    for (uint8_t i = 0; i < DHT22_MAXTIMINGS; i++) {
-		counter = count_seconds();
-		if (counter <= 100){
-			//1 Signal
-			//setze 1sen an die jeweiligen stellen
-			if(i < 8){
-				data[0] |= (1<<i);
-			}
-			else if(i < 16){
-				data[1] |= (1<<(i-8));
-			}
-			else if(i < 24){
-				data[2] |= (1<<(i-16));
-			}
-			else if(i < 32){
-				data[3] |= (1<<(i-24));
-			}
-			else if(i >= 32){
-				data[4] |= (1<<(i-32));
-			}
-		}
-		else if(counter == 212){
-			sei();
-			return 2;
-		}
-        else if (counter >= 250)
-			sei();
-            return i+5;
-    }
-
-    sei();
-    sum = data[0] + data[1] + data[2] + data[3];
-	if(sum != data[4]){
-		//zu letzt aendern
-		return 0;
-	}
-
-    return 1;
-}
-
-uint8_t DHT22::get_values(){
-	uint8_t status = read_sensor();
-	if (status == 1) {
-        /* Reading temperature */
-        temp = (int16_t)data[2]*256.0;
-		temp += data[3];
-        temp /= 10.0;
-
-		temperature_integral = (int8_t)data[2];
-		temperature_decimal = (uint8_t)data[3];
-
-        /* Reading humidity */
-        hum = data[0]*256.0;
-		hum += data[1];
-		hum /= 10.0;
-		
-		humidity_integral = (uint8_t)data[0];
-		humidity_decimal = (uint8_t)data[1];
-		
-    }
-    return status;
-}
+	
+} // end DHT22_StartReading
--- prog/wetter/wetterstation/main.cpp
+++ prog/wetter/wetterstation/main.cpp
@@ -7,11 +7,138 @@
 #include "BMP180.h"
 BMP180 druck;
 
-#define DDR_DHT22 DDRD
-#define PORT_DHT22 PORTD
-#define PIN_DHT22 PIND
 #include "DHT22.h"
 DHT22 dht22(2);
+
+/*
+ * Timer Compare Match interrupt handler
+ *
+ * This handler is used to generate host start conditions (Periods P1 and P2).
+ * Using a 8bit timer with prescaler such that a timer tick corresponds to 1us (freq. = 1MHz).
+ */
+ISR(TIMER_CTC_VECTOR){
+	
+	/* Using a 8bit timer maximum delay is 255us, we need at least 500us in Period P1.
+	   Se, we need two timer interrupts. We check this with overflow_cnt and comparing
+	   it the the define OVERFLOWS_HOST_START.
+	   We make the pin = 0 at the begining of the state machine (function DHT22_StartReading) */
+	if((dht22.state == dht22.DHT_HOST_START) && (dht22.overflow_cnt < (OVERFLOWS_HOST_START - 1))){
+		dht22.overflow_cnt++;
+	}
+	/* After Period P1, we need to hold the pin high for aprox. 40us. So, we change timer compare
+	   register to 40. */
+	else if((dht22.state == dht22.DHT_HOST_START) && (dht22.overflow_cnt >= (OVERFLOWS_HOST_START - 1))){ // 510us have passed.
+		PIN_HIGH(DHT22_PORT,DHT22_PIN); // Change pin to High for period P2.
+		dht22.overflow_cnt = 0;
+		dht22.state = dht22.DHT_HOST_PULLUP;
+		TIMER_OCR_REGISTER = 40;
+		return;
+	}
+	/* The Period P2 have passed. We need now to change the pin to input and wait for sensor
+	   to respond. External INT is used. Sensor will respond by pulling the line down for aprox. 
+	   80us. So, we can measure period P3 at the next rising edge interrupt. */
+	else if (dht22.state == dht22.DHT_HOST_PULLUP){ // more 40us have passed
+		TIMER_OCR_REGISTER = 255; // Change timer compare to 255, for now on, the timer interrupt should not fire.
+		                          // If if fires, too much time has passed and something is wrong. We will clear
+					              // the timer counter at the beggining of the external interrupt handler.
+		SET_PIN_INPUT(DHT22_DDR,DHT22_PIN); // Set pin as input.
+		PIN_HIGH(DHT22_PORT,DHT22_PIN); // Write 1 to enable pullup.
+		EXT_INTERRUPT_DISABLE  // Disable external interrupt (in case it is already enable)
+		EXT_INTERRUPT_SET_RISING_EDGE // Setting ext. int to rising edge.
+		EXT_INTERRUPT_CLEAR_FLAG // Clear flag to avoid spurious firing of ext. int.
+		EXT_INTERRUPT_ENABLE  // Re-enable external int.
+		TIMER_COUNTER_REGISTER = 0; // Reset counter
+		dht22.state = dht22.DHT_WAIT_SENSOR_RESPONSE; // Change state.
+		return; // Return of the int. handler.
+	}
+	/* If the timer interrupt fired while not in the previous states, than too much time
+	   has passed and we signal a error. */
+	else{ 
+		dht22.state = dht22.DHT_ERROR_NOT_RESPOND; // Change to a error state
+		TIMER_STOP // Stop timer.
+		EXT_INTERRUPT_DISABLE  // Disable external interrupt
+		SET_PIN_OUTPUT(DHT22_DDR,DHT22_PIN); // Set pin back to output.
+		PIN_HIGH(DHT22_PORT,DHT22_PIN); // Set pin high to disable DHT22.
+		dht22.bitcounter = 0; // reset bit counter.
+	}
+}
+
+/*
+ * External interrupt handler
+ * 
+ * The external interrupt is used to measure the width of a pulse and change
+ * the state accordingly.
+ */
+ISR(EXT_INTERRUPT_VECTOR){
+	
+	uint8_t counter_us;
+	counter_us = TIMER_COUNTER_REGISTER; // Store counter value
+	TIMER_COUNTER_REGISTER = 0; // Reset counter.
+	
+	/* Period P3. Sensor pulls down the line for aprox. 80us.
+	   The ext int. was configured to rising edge. If counter is aprox. 80,
+	   (or  < 100 in this case) when the line rises it
+	   indicates that the sensor responded.
+	   Now we have to change interrupt sense to falling edge in order to
+	   detect the period P4.
+	 */
+	if ((dht22.state == dht22.DHT_WAIT_SENSOR_RESPONSE && (counter_us > 40) && counter_us < 100)){ // Sensor responded (Period P3).
+		EXT_INTERRUPT_DISABLE  // Disabling interrupt.
+		EXT_INTERRUPT_SET_FALLING_EDGE  // Changing interrupt sense to falling edge.
+		EXT_INTERRUPT_CLEAR_FLAG  // clearing flag (this prevents interrupt to fire when changing to falling edge).
+		EXT_INTERRUPT_ENABLE  // Re-enabling interrupt.
+		dht22.state = dht22.DHT_SENSOR_PULLUP; // Changing state.
+		return;
+	}
+	/* Period P4. When the falling edge interrupt occurs, indicating the end of P4,
+	   we get the counter register and check it value. If it is less than 100 (period
+	   P4 is also aprox. 80us) then the sensor responded pulling up the line. Now the 
+	   bit transmission will start and we only need to measure the with of each bit. So,
+	   the external interrupt can stay on falling edge. */
+	else if((dht22.state == dht22.DHT_SENSOR_PULLUP) && (counter_us > 60) && (counter_us < 100)){ // Sensor responded (Period P4).
+		dht22.state = dht22.DHT_TRANSFERING; // Change state
+		return;
+	}
+	/* Period P5. Measuring the with of the pulse in order to determine if it is a 0 or a 1.
+	   Bit 0 has a period of 50us + 28us. So we check if it is larger than 50us and smaller than 110. (DHT22 timing is no precise, neither the timer) */
+	else if((dht22.state == dht22.DHT_TRANSFERING) && (counter_us > 50) && (counter_us <= 110)){ // Sensor sent a databit 0 (Period P5).
+		// If bit is a 0, only increment the bit counter (we need only to shift 1's).
+		dht22.bitcounter++; 
+	}
+	/* Period P5. Bit 1 has a period of 50us + 70us. So, we check if it is lager than 50us and smaller than 160. */
+	else if((dht22.state == dht22.DHT_TRANSFERING) && (counter_us > 110) && (counter_us <= 160)){ // Sensor sent a databit 1 (Period P5).
+		/* If bit is one, we shift one to the variables rawHumidity, rawTemperature and checkSum according
+		   with bit position givem by bitcounter */
+		if (dht22.bitcounter < 16) // Humidity
+		{
+			dht22.rawHumidity |= (1 << (15 - dht22.bitcounter));
+		}
+		if ((dht22.bitcounter > 15) && (dht22.bitcounter < 32))  // Temperature
+		{
+			dht22.rawTemperature |= (1 << (31 - dht22.bitcounter));
+		}
+		if ((dht22.bitcounter > 31) && (dht22.bitcounter < 40))  // CRC data
+		{
+			dht22.checkSum |= (1 << (39 - dht22.bitcounter));
+		}
+		dht22.bitcounter++; // Increments bit counter, the state does not change. 
+	}
+	
+	/* Check if all bits arrived. If so, stop the timer and external interrupt. */
+	if (dht22.bitcounter > 39){ // Transfer done
+		TIMER_STOP // Stop timer.
+		TIMER_COUNTER_REGISTER = 0; // Reset counter.
+		EXT_INTERRUPT_DISABLE // Disabling interrupt
+		SET_PIN_OUTPUT(DHT22_DDR,DHT22_PIN);
+		PIN_HIGH(DHT22_PORT,DHT22_PIN);
+		dht22.bitcounter = 0; // Reset bit counter.
+		dht22.state = dht22.DHT_CHECK_CRC; // Change state.
+	}
+	
+	/* CRC check is done at outside interrupt handler, by the
+	function DHT22_CheckStatus. This way, this handler is very fast. */
+				
+}
 /*
 extern "C"{
 	#include "DHT22int.c"
@@ -24,6 +151,7 @@ void send_weather();
 
 #define KONTUNUIRLICHER_MODUS 0
 uint8_t FLAG_REG;
+uint8_t status;
 
 ISR(USART_RX_vect){
 	uint8_t temp = UDR0;
@@ -88,15 +216,16 @@ int main(){
 	init();
 	
 	while(true){
-		wait(20);
+		wait(10);
 		PORTB |= (1<<PORTB0);
 		PORTB &= ~(1<<PORTB1);
-		wait(20);
+		wait(10);
 		PORTB |= (1<<PORTB1);
 		PORTB &= ~(1<<PORTB0);
 		if((FLAG_REG&(1<<KONTUNUIRLICHER_MODUS))){
 			send_weather();
 		}
+		status = dht22.DHT22_StartReading();
 	}
 	return 0;
 }
@@ -119,6 +248,9 @@ void init(){
 	
 	//Init BMP180
 	druck.bmp180_getcalibration();
+	
+	//starte erste Messung des dht22
+	dht22.DHT22_StartReading();
 
 	FLAG_REG = 0;
 	sei();
@@ -131,36 +263,38 @@ void transmit_values(unsigned char data){
 }
 
 void send_weather(){
-	uint8_t status;
 	druck.bmp180_getaltitude();
-	status = dht22.get_values();
-	if(dht22.temperature_integral<0){
-		transmit_values('-');
-		transmit_values('0'+(-1)*dht22.temperature_integral/10);
+	status = dht22.DHT22_CheckStatus();
+	if(status == dht22.DHT_DATA_READY){
+		if(dht22.temperature_integral<0){
+			transmit_values('-');
+			transmit_values('0'+(-1)*dht22.temperature_integral/10);
+		}
+		else{
+			transmit_values('+');
+			transmit_values('0'+dht22.temperature_integral/10);
+		}
+		transmit_values('0'+dht22.temperature_integral%10);
+		transmit_values('.');
+		transmit_values('0'+dht22.temperature_decimal);
+		transmit_values('\t');
+		transmit_values('0'+dht22.humidity_integral/100); 
+		transmit_values('0'+dht22.humidity_integral/10); 
+		transmit_values('0'+dht22.humidity_integral%10);
+		transmit_values('.');
+		transmit_values('0'+dht22.humidity_decimal);
+		transmit_values('\t');
+		transmit_values('0'+(int)druck.pressure/1000);
+		transmit_values('0'+(int)druck.pressure%1000/100);
+		transmit_values('0'+(int)druck.pressure%100/10);
+		transmit_values('0'+(int)druck.pressure%10);
+		transmit_values('.');
+		transmit_values('0'+(int)(druck.pressure*10.0)%10);
+		transmit_values('\t');
+		transmit_values('a'+status);
+		transmit_values('\n');
+		transmit_values('\r');
+		dht22.DHT22_StartReading();
 	}
-	else{
-		transmit_values('+');
-		transmit_values('0'+dht22.temperature_integral/10);
-	}
-	transmit_values('0'+dht22.temperature_integral%10);
-	transmit_values('.');
-	transmit_values('0'+dht22.temperature_decimal);
-	transmit_values('\t');
-	transmit_values('0'+dht22.humidity_integral/100); 
-	transmit_values('0'+dht22.humidity_integral/10); 
-	transmit_values('0'+dht22.humidity_integral%10);
-	transmit_values('.');
-	transmit_values('0'+dht22.humidity_decimal);
-	transmit_values('\t');
-	transmit_values('0'+(int)druck.pressure/1000);
-	transmit_values('0'+(int)druck.pressure%1000/100);
-	transmit_values('0'+(int)druck.pressure%100/10);
-	transmit_values('0'+(int)druck.pressure%10);
-	transmit_values('.');
-	transmit_values('0'+(int)(druck.pressure*10.0)%10);
-	transmit_values('\t');
-	transmit_values('a'+status);
-	transmit_values('\n');
-	transmit_values('\r');
 }
 
