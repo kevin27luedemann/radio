cf32e4446f1e744ee25b0581db1624db4e92f572
Kevin Luedemann <kevin27luedemann@gmail.com>
2016-08-26 19:12:59 +0200

    namensaenderung

--- prog/wetter/auslesesoftware/pi_get_weather.sh
+++ prog/wetter/auslesesoftware/pi_get_weather.sh
@@ -1,9 +1,9 @@
-stty 9600 -F /dev/ttyUSB0
-echo "da" > /dev/ttyUSB0
-while read -r line < /dev/ttyUSB0; do
+stty 9600 -F /dev/ttyAMA0
+echo "da" > /dev/ttyAMA0
+while read -r line < /dev/ttyAMA0; do
    datum=`date +'%d.%m.%Y %H:%M:%S'`
 	echo -e $datum "\t" $line >> "/home/pi/.wetterstation.log"
 	echo -e $datum "\t" $line > "/home/pi/.wetterstation_current.log"
 	sleep 20
-	echo "da" > /dev/ttyUSB0
+	echo "da" > /dev/ttyAMA0
 done
