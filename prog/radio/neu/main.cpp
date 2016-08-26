039df66d51bf4bd6f13a7a1bd0c27612b3504064
Kevin Luedemann <kevin27luedemann@gmail.com>
2016-08-26 21:30:39 +0200

    programm mit neuer Hardware funktioniert

--- prog/radio/neu/main.cpp
+++ prog/radio/neu/main.cpp
@@ -466,14 +466,14 @@ static gboolean update_datescreen(gpointer data){
 			gtk_widget_hide(GTK_WIDGET(window_black));
 		}
 	}
-	if(!OFFSCREEN && NACHTS && counter_off >= 420){
+	if(!OFFSCREEN && NACHTS && counter_off >= 42){
 		OFFSCREEN = true;
 		gtk_widget_show(GTK_WIDGET(window_off1));
 #ifndef demo
 		BACKLIGHTAUS();
 #endif
 	}
-	else if(!OFFSCREEN && !NACHTS && counter_off >= 420){
+	else if(!OFFSCREEN && !NACHTS && counter_off >= 42){
 		OFFSCREEN = true;
 		gtk_widget_show(GTK_WIDGET(window_off1));
 	}
--- prog/wetter/auslesesoftware/pi_get_weather.sh
+++ prog/wetter/auslesesoftware/pi_get_weather.sh
@@ -2,8 +2,8 @@ stty 9600 -F /dev/ttyAMA0
 echo "da" > /dev/ttyAMA0
 while read -r line < /dev/ttyAMA0; do
    datum=`date +'%d.%m.%Y %H:%M:%S'`
-	echo -e $datum "\t" $line >> "/home/pi/.wetterstation.log"
-	echo -e $datum "\t" $line > "/home/pi/.wetterstation_current.log"
-	sleep 20
+	echo $datum "\t" $line >> "/home/pi/.wetterstation.log"
+	echo $datum "\t" $line > "/home/pi/.wetterstation_current.log"
+	sleep 60
 	echo "da" > /dev/ttyAMA0
 done
