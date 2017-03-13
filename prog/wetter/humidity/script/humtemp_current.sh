01e9e81afbafcbd7a01628ad15e4a8cc4e443e32
kevin.luedemann <kevin.luedemann@stud.uni-goettingen.de>
2017-03-13 16:19:36 +0100

    new humtemp script

---
+++ prog/wetter/humidity/script/humtemp_current.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# grundlegende Zeitvariablen
+datum=`date +"%d.%m.%y %H:%M:%S"`
+ 
+GPIO=7
+INPUT=$(sudo /home/pi/.lol_dht22/loldht $GPIO | grep "Temperature")
+HUM=$(echo $INPUT|cut -d " " -f3)
+TEMP=$(echo $INPUT|cut -d " " -f7)
+
+
+#echo -e "$datum\t$TEMP\t$HUM" >> /home/pi/.wetter.log ;
+echo -e "$TEMP#$HUM#" > /home/pi/.wetter.status ;
+echo -e "${datum}\t${TEMP}\t${TEMP}\t${HUM}\t${HUM}\t0000.0" > "/home/pi/.wetterstation_current.log"
