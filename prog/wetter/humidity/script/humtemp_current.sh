9c64c298870a410f1cbd1aa718ec8eee138eebe2
kevin.luedemann <kevin.luedemann@stud.uni-goettingen.de>
2017-03-13 20:47:12 +0100

    bug fix

--- prog/wetter/humidity/script/humtemp_current.sh
+++ prog/wetter/humidity/script/humtemp_current.sh
@@ -4,10 +4,14 @@ datum=`date +"%d.%m.%y %H:%M:%S"`
  
 GPIO=7
 INPUT=$(sudo /home/pi/.lol_dht22/loldht $GPIO | grep "Temperature")
+echo $INPUT
 HUM=$(echo $INPUT|cut -d " " -f3)
 TEMP=$(echo $INPUT|cut -d " " -f7)
+
+echo ${HUM:0:4}
+echo ${TEMP:0:4}
 
 
 #echo -e "$datum\t$TEMP\t$HUM" >> /home/pi/.wetter.log ;
 echo -e "$TEMP#$HUM#" > /home/pi/.wetter.status ;
-echo -e "${datum}\t${TEMP}\t${TEMP}\t${HUM}\t${HUM}\t0000.0" > "/home/pi/.wetterstation_current.log"
+echo -e "${datum}\t${TEMP:0:4}\t${TEMP:0:4}\t${HUM:0:4}\t${HUM:0:4}\t0000.0" > "/home/pi/.wetterstation_current.log"
